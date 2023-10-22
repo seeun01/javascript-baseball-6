532b826a076363e47259cd14ee863edbe70560b4
seeun <ssky601@gmail.com>
2023-10-23 03:11:36 +0900

    feat: 서로 다른 3자리의 수 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
   play(){
     Console.print("숫자 야구 게임을 시작합니다.")
+    this.ComputerCreateNumber();
   }
+
+  ComputerCreateNumber(){
+    const computerArr = [];
+    while(computerArr.length < 3){
+      const number = Random.pickNumberInRange(1,9);
+      if(!computerArr.includes(number)){
+        computerArr.push(number);
+      }
+    }
+    const computer = computerArr.join('');
+    Console.print(computer);
+  } 
 }
 let app = new App();
 app.play(); //숫자 야구 게임을 시작합니다.
