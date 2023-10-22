53ba47f61c20fe01da182199ab0b18668e4b51a0
seeun <ssky601@gmail.com>
2023-10-23 02:05:59 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async play() {}
+  play(){
+    Console.print("숫자 야구 게임을 시작합니다.")
+  }
 }
+let app = new App();
+app.play(); //숫자 야구 게임을 시작합니다.
 
 export default App;
